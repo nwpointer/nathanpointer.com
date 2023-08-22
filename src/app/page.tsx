76e8db1f9662a591ed62f8a29aa8824b5e0ead09
dev2 <dev2@Nathans-MacBook-Pro.local>
2023-08-21 22:26:18 -0700

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,86 @@
-import Image from 'next/image'
+import { ModeToggle } from "@/components/theme-toggle"
+import { Button } from "@/components/ui/button"
+
+function Logo() {
+  return <div className="flex items-center gap-4">
+    <img className="rounded-full h-10" src="face.png" alt="" />
+    <div>
+      <span className="block leading-tight">Nathan Pointer</span>
+      <span className="block leading-tight text-xs text-muted-foreground opacity-70">Web developer</span>
+    </div>
+  </div>
+}
+
+function MainNav() {
+  return (
+    <div className="flex justify-between py-4 pt-8 container">
+      <Logo />
+      <div className="flex items-center gap-2">
+        <Button variant="link">blog</Button>
+        <Button variant="link">contact</Button>
+        <ModeToggle />
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <section>
+        <MainNav />
+      </section>
+      <section className="min-h-[500px] flex items-center">
+        <div className="container">
+          <div className="max-w-3xl">
+            <h1 className="text-4xl leading-snug">I'm an award winning <b>web developer</b> from Oregon. I specialize in engineering <b>event driven micro services</b> and <b>react.js applications</b>.</h1>
+            <br />
+            <h2 className="text-2xl">I occasionally tweet and like to blog.</h2>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </section>
+      <section>
+        <div className="container grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="col-span-2 max-w-md">
+            <h3 className="text-xl uppercase mb-2 tracking-wide font-semibold">about</h3>
+            <p className="text-muted-foreground mb-4 leading-relaxed">
+              Im pragmatic, creative and care deeply about quality software - a passion I picked up early when I was still in Highschool.
+            </p>
+            <p className="text-muted-foreground mb-4 leading-relaxed">
+              I love a good challenge and I find I'm often at my happiest when designing technical solutions to novel problems with an engaged and empowered team of peers.
+            </p>
+            <p className="text-muted-foreground mb-4 leading-relaxed">
+              I live in Portand Oregon with my beautiful girlfriend and my cat named pickles
+            </p>
+            <br />
+            <h3 className="text-nd capitalize mb-2 tracking-wide font-semibold">Interests:</h3>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Dungeons and Dragons</div>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Personal Fitness</div>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Philosophy</div>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Graphic Design</div>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Video games</div>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Tiny Houses</div>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Etymology</div>
+              <div className="bg-muted rounded-full inline-block border mr-2 mb-2 p-1 px-4 text-sm text-muted-foreground hover:-top-1 shadow-sm hover:shadow-md relative">Dad jokes</div>
+          </div>
+          <div className="flex gap-6 flex-col">
+            <div>
+              <h3 className="text-xl uppercase mb-2 tracking-wide font-semibold">Open Source</h3>
+              three-landscape
+              <br /><br />
+            </div>
+            <div>
+              <h3 className="text-xl uppercase mb-2 tracking-wide font-semibold">Writing</h3>
+              <div className="text-xs text-muted-foreground mb-2 mt-6">August 2021</div>
+              <a className="text-sm hover:underline" href="/">Rendering semi-realistic Landscapes in the browser</a>
+              <div className="text-xs text-muted-foreground mb-2 mt-6">August 2021</div>
+              <a className="text-sm hover:underline" href="/">Rendering semi-realistic Landscapes in the browser</a>
+              <div className="text-xs text-muted-foreground mb-2 mt-6">August 2021</div>
+              <a className="text-sm hover:underline" href="/">Rendering semi-realistic Landscapes in the browser</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
