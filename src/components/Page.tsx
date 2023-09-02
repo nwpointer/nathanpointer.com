058da17f3c4316d724bbd8bc53e7172466f53bca
dev2 <dev2@Nathans-MacBook-Pro.local>
2023-09-02 15:29:42 -0700

    subtle blue

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,12 +5,16 @@ export default function Page({ children, ...args }: any) {
     return (
         <div {...args} >
             <Flourish />
+            <Flourish />
             <section>
                 <MainNav />
             </section>
             <section >
                 {children}
             </section>
+            <div className="fixed w-full max-w-screen -z-10 bottom-3">
+                <div className="bottom-fade w-full border"></div>
+            </div>
         </div >
     )
 }
